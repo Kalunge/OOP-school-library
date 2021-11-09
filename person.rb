e0876ae2ba2a2d623488aab3da0aa034b42ef685
kalunge <muthomititus+github@gmail.com>
2021-11-09 12:21:15 +0100

    Add validate_name method to person using Corrector instance

--- person.rb
+++ person.rb
@@ -1,9 +1,12 @@
+require_relative 'corrector'
+
 class Person
   def initialize(age, name = 'Unknown', parent_permission: true)
     @id = Random.rand(1..1000)
     @age = age
     @name = name
     @parent_permission = parent_permission
+    @correct_name = Corrector.new
   end
 
   attr_accessor :name, :age
@@ -15,6 +18,10 @@ class Person
 
   def can_use_services?
     @age >= 18 || @parent_permission
+  end
+
+  def validate_name
+    @name = @correct_name.corrector(@name)
   end
 
   private :of_age?
