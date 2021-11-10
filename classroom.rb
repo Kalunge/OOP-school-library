857cd8539b40da4fa427d365b77d2b879180447b
kalunge <muthomititus+github@gmail.com>
2021-11-10 12:23:02 +0100

    Remove commented code

--- classroom.rb
+++ classroom.rb
@@ -11,9 +11,4 @@ class ClassRoom
     @students.push(student)
     student.classroom = self
   end
-  # Create the has-many/belongs-to relationship between Classroom and Student. The following should be implemented:
-  # Create the has-many side (a classroom has many students).
-  # Create the belongs-to side (a student belongs to a classroom).
-  # Make sure that when adding a student to a classroom it also sets the classroom for the student.
-  # Make sure that when setting the classroom for a student it also adds it to the classrooms' students.
 end
