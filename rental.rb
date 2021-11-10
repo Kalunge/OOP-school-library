eafd2170aa358da781363b85a4af7dd9d243828d
kalunge <muthomititus+github@gmail.com>
2021-11-10 15:16:30 +0100

    Add setters for rentals

--- rental.rb
+++ rental.rb
@@ -1,5 +1,6 @@
 class Rental
-  attr_accessor :date, :book, :person
+  attr_accessor :date
+  attr_reader :book, :person
 
   def initialize(date, book, person)
     @date = date
@@ -9,5 +10,15 @@ class Rental
 
     @person = person
     person.rentals << self
+  end
+
+  def book=(book)
+    @book = book
+    book.add_rental(self)
+  end
+
+  def person=(person)
+    @person = person
+    person.add_rental(self)
   end
 end
